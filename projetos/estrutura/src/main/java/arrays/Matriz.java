dc05b15198e13c67c9f13410c7072e04d8c6c3ea
gabsmaciel <gabriellammaciel@hotmail.com>
2023-05-29 11:20:48 -0300

    estudos equals e hashcode

--- projetos/estrutura/src/main/java/arrays/Matriz.java
+++ projetos/estrutura/src/main/java/arrays/Matriz.java
@@ -1,29 +1,37 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Matriz {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Quantos alunos? ");
-        int qtdeAlunos = entrada.nextInt();
+    public static void main(String args[] ) {
 
-        System.out.println("Quantas notas por alunos? ");
-        int qtdeNotas = entrada.nextInt();
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("Digite os números separados: ");
+        String entradaUsuario = entrada.nextLine();
 
-        double[][] notasDaTurma = new double[qtdeAlunos][qtdeNotas];
+        String [] numeros = entradaUsuario.split(",");
+        int [] vetor = Arrays.stream(numeros).
+       mapToInt(Integer::parseInt)
+                .toArray();
+        //utilização de lambdas
 
-        double total = 0;
-        for (int i = 0; i < notasDaTurma.length; i++) {
-            for (int n = 0; n < notasDaTurma[i].length; n++) {
-                System.out.printf("Informe a nota %d do aluno %d: ", i + 1, n + 1);
-                notasDaTurma[i][n] = entrada.nextDouble();
-                total += notasDaTurma[i][n];
-
+        int nRepetidos = contaNumerosRepetidos(vetor);
+        System.out.println ("Quantidade de números repetidos: " + nRepetidos);
+    }
+    public static int contaNumerosRepetidos(int[] vetor) {
+        int contador = 0;
+        HashSet<Integer> numerosRepetidos = new HashSet<>();
+        for (int i =0; i < vetor.length; i++) {
+            for (int j = i + 1; j < vetor.length; j++) {
+                if (vetor [i] == vetor [j]&& !numerosRepetidos.contains(vetor[i])) {
+                    contador++;
+                    numerosRepetidos.add(vetor [i]);
+                    break;
+                }
             }
         }
-        double media = total / (qtdeAlunos * qtdeNotas);
-        System.out.println("A média da turma é: " + media);
-        entrada.close();
+        return contador;
     }
 }
