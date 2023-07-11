7fbbf6ec0607f5d06b5ddc730287c063c2a8ac63
gabsmaciel <gabriellammaciel@hotmail.com>
2023-07-11 13:38:43 -0300

    estudos de polimorfismo e em seguida interface

--- projetos/estrutura/src/main/java/oo/encapsulamento/Pessoa.java
+++ projetos/estrutura/src/main/java/oo/encapsulamento/Pessoa.java
@@ -1,6 +1,37 @@
 package oo.encapsulamento;
 
 public class Pessoa {
-    public int idade;
+    private int idade;
+    private String nome;
 
+    public String getNome() {
+        return nome;
+    }
+    public Pessoa(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    ///Getter
+    public int getIdade() {
+        return idade;
+    }
+
+    ////Setter
+    public void setIdade(int novaIdade) {
+        novaIdade = Math.abs(novaIdade);
+        if (novaIdade >= 0 && novaIdade <= 120) {
+            this.idade = novaIdade;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Olá eu sou " + getNome() +
+                 " e tenho " + getIdade() + " anos.";
+    }
 }
--- projetos/estrutura/src/main/java/oo/encapsulamento/PessoaTeste.java
+++ projetos/estrutura/src/main/java/oo/encapsulamento/PessoaTeste.java
@@ -1,12 +1,14 @@
 package oo.encapsulamento;
 
+import java.io.InvalidClassException;
+
 public class PessoaTeste {
 
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa();
-        p1.idade = -30; //alterar valor da variavel
-        System.out.println(p1.idade);  ///le o valor da variavel.
-        
-
+        Pessoa p1 = new Pessoa("Maria", 34);
+        p1.setIdade(30);
+        // p1.idade = -30; //alterar valor da variavel - estava public
+        // System.out.println(p1.idade);  ///le o valor da variavel.
+        System.out.println(p1); //toString
     }
 }
